974849cbf2a63bc6ad0dfcd63f31ac63134866f0
Ronte's Laptop <ronte.parker@bison.howard.edu>
2022-03-05 02:05:30 -0500

    Finished Test and Edge Cases Code Checking

--- LSP_2022-Assignment1-Hello_World/src/org/howard/edu/lsp/assignment5/test/Driver.java
+++ LSP_2022-Assignment1-Hello_World/src/org/howard/edu/lsp/assignment5/test/Driver.java
@@ -18,13 +18,142 @@ public class Driver {
 		System.out.println("SetA: "+ setA.toString());
 		System.out.println("SetB: "+ setB.toString());
 		System.out.println();
+		
 		System.out.println("True or False: SetA contains the integer 3.");
 		System.out.println(setA.contains(3));
 		System.out.println();
-		System.out.println("True or False: SetA contains the integer 3.");
+		System.out.println("True or False: SetB contains the integer 3.");
 		System.out.println(setB.contains(3));
+		System.out.println("\n");
+		
+		System.out.println("Removing 3 from SetB...");
+		try {
+			setB.remove(3);
+		} catch (Exception e) {
+			System.out.println("A problem occurred: "+e);
+		}
+		
+		System.out.println("True or False: SetB contains the integer 3.");
+		System.out.println(setB.contains(3));
+		System.out.println("SetB: "+ setB.toString());
+		System.out.println("\n");
+		
+		System.out.println("Testing Removing Not Present Integers:");
+		System.out.println("Removing 3 from SetB again...");
+		try {
+			setB.remove(3);
+		} catch (Exception e) {
+			System.out.println("A problem occurred: "+e);
+		}
+		System.out.println("SetB: "+ setB.toString());
+		System.out.println("\n");
+		
+		System.out.println("Testing Removing An Integer From An Empty Integer Set:");
+		System.out.println("Clearing SetB...");
+		setB.clear();
+		System.out.println("SetB: "+ setB.toString());
 		System.out.println();
 		
+		System.out.println("Attempting to remove 3 from SetB again...");
+		try {
+			setB.remove(3);
+		} catch (Exception e) {
+			System.out.println("A problem occurred: "+e);
+		}
+		System.out.println("\n");
+		
+		System.out.println("Testing Adding Duplicate Integers:");
+		System.out.println("SetA: "+ setA.toString());
+		System.out.println();
+		
+		System.out.println("What is the length of setA?");
+		System.out.println(setA.length());
+		System.out.println();
+
+		System.out.println("Adding 1 to setA...");
+		setA.add(1);
+		System.out.println();
+
+		System.out.println("What is the length of setA?");
+		System.out.println(setA.length());
+		System.out.println("SetA: "+ setA.toString());
+		System.out.println("\n");
+		
+		setA.clear();
+		setA.add(0);
+		setA.add(1);
+		setA.add(2);
+		setA.add(3);
+		setA.add(4);
+		System.out.println("SetA: "+ setA.toString());
+		System.out.println("SetB: "+ setB.toString());
+		System.out.println("What are the largest and smallest values of setA, respectively?");
+		try {
+			System.out.println(setA.largest() + ", " + setA.smallest());
+		} catch (Exception e) {
+			System.out.println("An error occurred: "+e);
+		}
+		System.out.println();
+		System.out.println("What are the largest and smallest values of setB, respectively?");
+		try {
+			System.out.println(setB.largest() + ", " + setB.smallest());
+		} catch (Exception e) {
+			System.out.println("An error occurred: "+e);
+		}
+		System.out.println("\n");
+		
+		setB.add(2);
+		setB.add(3);
+		setB.add(4);
+		setB.add(5);
+		setB.add(6);
+		System.out.println("Set Comparisons:");
+		System.out.println("SetA: "+ setA.toString());
+		System.out.println("SetB: "+ setB.toString());
+		System.out.println();
+		
+		System.out.println("True or False: SetA equals SetB.");
+		System.out.println(setA.equals(setB));
+		System.out.println();
+		
+		System.out.println("What is SetA 'Union' SetB?");
+		setA.union(setB);
+		System.out.println("SetA 'Union' SetB = " + setA.toString());
+		setA.clear();
+		setA.add(0);
+		setA.add(1);
+		setA.add(2);
+		setA.add(3);
+		setA.add(4);
+		System.out.println("What is SetA 'Intersection' SetB?");
+		setA.intersect(setB);
+		System.out.println("SetA 'Intersection' SetB = " + setA.toString());
+		System.out.println();
+		setA.clear();
+		setA.add(0);
+		setA.add(1);
+		setA.add(2);
+		setA.add(3);
+		setA.add(4);
+		System.out.println("What is SetA 'Difference1' SetB?");
+		setA.diff1(setB);
+		System.out.println("SetA 'Difference1' SetB = " + setA.toString());
+		System.out.println();
+		setA.clear();
+		setA.add(0);
+		setA.add(1);
+		setA.add(2);
+		setA.add(3);
+		setA.add(4);
+		System.out.println("What is SetA 'Difference2' SetB?");
+		setA.diff2(setB);
+		System.out.println("SetA 'Difference2' SetB = " + setA.toString());
+		System.out.println();
+		
+		System.out.println("There are 2 different Difference Methods because i was unsure which way was meant.\n" +
+		"I made Difference1 to be similar to arithmetic difference where it removes all \nelements in SetB from SetA. "
+		+ "I made Difference2 to be where it takes the elements \nthat are not present in both sets and makes a new "
+		+ "with those elements.");
 	}
 
 }
